ee01a53f25c6b5f4cf6abb5dd968c03dd62d76fb
Lau Llobet <lau.llobet@gmail.com>
2016-03-16 13:44:16 +0100

    0.1WalkingSkeleton [11]  windows manager :20

--- src/test/java/UI/SymmetricResizableFrameTest.java
+++ src/test/java/UI/SymmetricResizableFrameTest.java
@@ -3,17 +3,22 @@ package UI;
 import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.util.ArrayList;
 
 /**
  * Created by quest on 15/3/16.
  */
 public class SymmetricResizableFrameTest {
     public static void main(String[] args) throws Exception {
+        
+
         Frame frame = new SymmetricResizableFrame("Symmetric",100,200,100,100);
         frame.setVisible(true); // make window visible
 
         Frame frame2 = new SymmetricResizableFrame("Symmetric",400,200,100,100);
         frame2.setVisible(true); // make window visible
+
+
     }
 
 }
