5d428f672939e638d4c227f3a88e60be295a4b30
Lau Llobet <lau.llobet@gmail.com>
2016-04-14 12:17:27 +0200

    0.8OpenCv [23]  colour extraction

--- src/main/java/ImageMatcherHelper.cpp
+++ src/main/java/ImageMatcherHelper.cpp
@@ -2,6 +2,14 @@
 #include <stdio.h>
 
 double* calculate(int *obs,int *mod){
-    printf("gogoogo");
+
+    int argb = obs[0];
+    int r = (argb)&0xFF;
+    int g = (argb>>8)&0xFF;
+    int b = (argb>>16)&0xFF;
+    int a = (argb>>24)&0xFF;
+
+    printf("---> r %d g %d b %d as %d\n",r,g,b,a);
+
     return NULL;
 }
